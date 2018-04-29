4053238266a480c21484e256e76f7082cd3c5994
Dermn8er <33468194+Dermn8er@users.noreply.github.com>
2018-04-28 21:17:20 -0600

    Update requirements.java

--- src/SAP/requirements.java
+++ src/SAP/requirements.java
@@ -1,5 +1,35 @@
 package SAP;
 
-public class requirements {
+import java.util.ArrayList;
 
+public class requirements {
+	public ArrayList<course> fulfill = new ArrayList<course>();
+	public String desc;
+	public int creditHours;
+	
+	public requirements(String s) {
+		desc = s;
+	}
+
+	public void setNeededCredit(int i) {
+		creditHours = i;
+	}
+
+	public void add(course temp) {
+		fulfill.add(temp);
+	}
+
+	public String getDesc() {
+		return desc;
+	}
+
+	public int size() {
+		return fulfill.size();
+	}
+
+	public course getCourse(int i) {
+		return fulfill.get(i);
+	}
+	
+	
 }
