1bda0a6f984f9f69122515700521954cbdd70ccf
hazermaveth <38387958+hazermaveth@users.noreply.github.com>
2018-05-06 16:58:35 -0600

    Update advisor.java

--- src/SAP/advisor.java
+++ src/SAP/advisor.java
@@ -1,4 +1,4 @@
-package SAP;
+
 
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -12,7 +12,7 @@ public class advisor{
 	String major;
 	char semester;
 	int totalCredit, year = 0;
-	int upperClass = 60;
+	int upperDevClass = 300, upperClass = 60, upperDevCredit = 0, upperDevCreditNeed = 40;
 	boolean odd;
 
 	/***
@@ -27,11 +27,40 @@ public class advisor{
 		reqComplete();
 		preReqMet();
 		offThisYear();
-		//offThisSemester();
+		offThisSemester();
+		order();
 		System.out.print(stringReq());
 	}
 
 
+	/**
+	 * Orders the req list by priority
+	 */
+	private void order() {
+		boolean swapped = true;
+		while(swapped){
+			swapped = false;
+			for (int i = 0; i < req.size()-1; i++){
+				if((req.get(i)).getpriority() < (req.get(i+1)).getpriority()){
+					requirements temp = req.get(i);
+					req.set(i, (req.get(i+1)));
+					req.set(i+1, temp);
+					swapped = true;
+				} 
+			}
+		}
+	}
+
+
+
+
+
+
+
+
+	/**
+	 * Checks if the class is offered in the selected year.
+	 */
 	private void offThisYear() {
 		for(int i = 0; i < req.size(); i++){
 			(req.get(i)).checkYear(year);	
@@ -49,7 +78,10 @@ public class advisor{
 		}
 		notDone.clear();
 	}
-	
+
+	/**
+	 * Checks if the class is offered in the selected semester.
+	 */
 	private void offThisSemester() {
 		for(int i = 0; i < req.size(); i++){
 			(req.get(i)).checkSemester(semester);	
@@ -133,7 +165,7 @@ public class advisor{
 	 */
 	public void majorProgress() {
 		List<String> classesTaken = new ArrayList<String>();
-
+		classesTaken = Arrays.asList("MATH 693, F, 0.0");
 		if (studentID.equals("F338258016")){
 			classesTaken = Arrays.asList("MATH 119, C, 4.0", "IS 301, B, 3.0", "IS 281, A, 3.0", "ENG 305, B, 3.0", "EET 340, B, 3.0"
 					, "CS 102, A, 1.0", "IS 315, A, 3.0", "CS 451, A, 3.0", "CS 234, A, 4.0", "ANTH 310, B, 3.0", "MATH 132, D, 4.0", "MATH 132, B, 4.0", 
@@ -143,9 +175,9 @@ public class advisor{
 					, "CS 102, A, 1.0", "IS 315, A, 3.0", "ENG 102, A, 3.0", "CS 234, A, 4.0", "ANTH 310, B, 3.0", "MATH 132, D, 4.0", "MATH 132, B, 4.0", 
 					"IS 343, A, 3.0", "IS 316, A, 3.0", "CS 360, A, 3.0", "CS 357, A, 3.0", "CS 301, B, 3.0", "CS 472, A, 3.0", "CS 461, A, 3.0", "SOC 101, B, 3.0");
 		} else if (studentID.equals("F438688089")){
-			classesTaken = Arrays.asList("MATH 119, C, 4.0", "IS 301, B, 3.0", "IS 281, A, 3.0", "ENG 305, B, 3.0", "EET 340, B, 3.0"
-					, "ENG 102, A, 1.0", "COMM 104, A, 3.0", "CS 451, A, 3.0", "CS 234, A, 4.0", "ANTH 310, B, 3.0", "MATH 132, D, 4.0", "MATH 132, B, 4.0", 
-					"IS 343, A, 3.0", "IS 316, A, 3.0", "CS 360, A, 3.0", "CS 357, A, 3.0", "CS 301, B, 3.0", "CS 472, A, 3.0", "UNIV 101, A, 3.0", "SOC 101, B, 3.0", "PSY 101, B, 3.0");
+			classesTaken = Arrays.asList("MATH 124, C, 4.0", "IS 301, B, 3.0", "IS 281, A, 3.0", "ENG 104, B, 3.0", "EET 340, B, 3.0"
+					, "CS 102, A, 1.0", "IS 315, A, 3.0", "ENG 102, A, 3.0", "CS 234, A, 4.0", "ANTH 310, B, 3.0", "MATH 132, D, 4.0", "MATH 132, B, 4.0", 
+					"IS 343, A, 3.0", "ART 131, A, 3.0", "CS 360, A, 3.0", "CS 357, A, 3.0", "CDIS 244, B, 3.0", "CS 472, A, 3.0", "CS 461, A, 3.0", "SOC 101, B, 3.0");
 		} else if (studentID.equals("F931238613")){
 			classesTaken = Arrays.asList("MATH 124, C, 4.0", "IS 301, B, 3.0", "IS 281, A, 3.0", "ENG 305, B, 3.0", "EET 340, B, 3.0"
 					, "CS 102, A, 1.0", "IS 315, A, 3.0", "CS 451, A, 3.0", "CS 234, A, 4.0", "ANTH 310, B, 3.0", "MATH 132, D, 4.0", "MATH 132, B, 4.0", 
@@ -161,6 +193,9 @@ public class advisor{
 				int classCH = Integer.parseInt(currentCHours);
 				totalCredit += classCH;
 				course class1 = new course(className);
+				if(class1.getIDNum()> upperDevClass){
+					upperDevCredit += classCH;
+				}
 				class1.setGrade(classGrade);
 				class1.setHours(classCH);
 				courses.add(class1);	
@@ -175,11 +210,25 @@ public class advisor{
 		List<String> requirmentsList = Arrays.asList("UNIV 101 Freshman Seminar, 3, UNIV 101, B, A, D, 3", "Communicating Effectively (ENG 102), 3, ENG 102, B, A, C, 3", "Communicating Effectively (ENG 104), 3, ENG 104, B, A, C, 3",
 				"Communicating Effectively (COMM), 3, COMM 101, B, A, D, 3", "Communicating Effectively (COMM), 3, COMM 102, B, A, D, 3", "Communicating Effectively (COMM), 3, COMM 202, B, A, D, 3",
 				"Understanding and Applying Mathematical Principles, 3, MATH 113, B, A, D, 3", "Understanding and Applying Mathematical Principles, 3, MATH 119, B, A, D, 3",
-				"Understanding and Applying Mathematical Principles, 3, MATH 120, B, A, D, 3", "Understanding and Applying Mathematical Principles, 3, MATH 124, B, A, D, 3", 
-				"Understanding and Applying Mathematical Principles, 3, STAT 213, B, A, D, 3", "Social Science (Choose Two From Different Disciplines), 6, ANTH 105, S, A, D, 3", "Social Science (Choose Two From Different Disciplines), 6, ANTH 243, B, A, D, 3", "Social Science (Choose Two From Different Disciplines), 6, ANTH/GEOG 103, B, A, D, 3",
+				"Understanding and Applying Mathematical Principles, 3, MATH 120, B, A, D, 3", "Understanding and Applying Mathematical Principles, 3, MATH 124, B, A, D, 3, MATH 120", 
+				"Understanding and Applying Mathematical Principles, 3, STAT 213, B, A, D, 3", "Science (Lab has to be taken with course), 4, ANTH 245, B, A, D, 4", "Social Science (Choose Two From Different Disciplines), 6, ANTH 105, S, A, D, 3", "Social Science (Choose Two From Different Disciplines), 6, ANTH 243, B, A, D, 3", "Social Science (Choose Two From Different Disciplines), 6, ANTH/GEOG 103, B, A, D, 3",
 				"Social Science (Choose Two From Different Disciplines), 6, ANTH/GEOG 233, B, A, D, 3", "Social Science (Choose Two From Different Disciplines), 6, ECON 200, B, A, D, 3", "Social Science (Choose Two From Different Disciplines), 6, ECON 221, B, A, D, 3", "Social Science (Choose Two From Different Disciplines), 6, ECON 222, B, A, D, 3", "Social Science (Choose Two From Different Disciplines), 6, FCS 221, F, A, D, 3",
 				"Social Science (Choose Two From Different Disciplines), 6, PSCI 101, B, A, D, 3", "Social Science (Choose Two From Different Disciplines), 6, PSCI 102, B, A, D, 3", "Social Science (Choose Two From Different Disciplines), 6, PSY 101, B, A, D, 3", "Social Science (Choose Two From Different Disciplines), 6, PSY 201, B, A, D, 3", "Social Science (Choose Two From Different Disciplines), 6, PSY 202, B, A, D, 3",
-				"Social Science (Choose Two From Different Disciplines), 6, SOC 101, B, A, D, 3", "Social Science (Choose Two From Different Disciplines), 6, SOC 212, S, A, D, 3", "Social Science (Choose Two From Different Disciplines), 6, SOC 215, F, A, D, 3");
+				"Social Science (Choose Two From Different Disciplines), 6, SOC 101, B, A, D, 3", "Social Science (Choose Two From Different Disciplines), 6, SOC 212, S, A, D, 3", "Social Science (Choose Two From Different Disciplines), 6, SOC 215, F, A, D, 3", "Fine Arts (3-hours min-6 hours max), 3, ART 101, B, A, D, 3", "Fine Arts (3-hours min-6 hours max), 3, ART 131, B, A, D, 3",
+				"Fine Arts (3-hours min-6 hours max), 3, ART 135, F, A, D, 3", "Fine Arts (3-hours min-6 hours max), 3, ART 165, F, A, D, 3", "Fine Arts (3-hours min-6 hours max), 3, ART 166, S, A, D, 3", "Fine Arts (3-hours min-6 hours max), 3, ART 210, B, A, D, 3", "Fine Arts (3-hours min-6 hours max), 3, ART 231, B, A, D, 3", "Fine Arts (3-hours min-6 hours max), 3, ART 271, B, A, D, 3",
+				"Fine Arts (3-hours min-6 hours max), 3, DFM 105, F, A, D, 3", "Fine Arts (3-hours min-6 hours max), 3, DNC 101, B, A, D, 3", "Fine Arts (3-hours min-6 hours max), 3, DNC 210, B, A, D, 3", "Fine Arts (3-hours min-6 hours max), 3, MUS 101, B, A, D, 3", "Fine Arts (3-hours min-6 hours max), 3, MUS 113, B, A, D, 3", "Fine Arts (3-hours min-6 hours max), 3, MUS 163, S, A, D, 3",
+				"Fine Arts (3-hours min-6 hours max), 3, MUS 241, F, A, D, 3", "Fine Arts (3-hours min-6 hours max), 3, THTR 111, F, A, D, 3", "Fine Arts (3-hours min-6 hours max), 3, THTR 113, F, A, D, 3", "Fine Arts (3-hours min-6 hours max), 3, THTR 121, F, A, D, 3", "Humanities (3-hours min-6 hours max), 3, CDIS 144, F, A, D, 3", "Humanities (3-hours min-6 hours max), 3, CDIS 244, F, A, D, 3",
+				"Humanities (3-hours min-6 hours max), 3, ENG 205, F, A, D, 3", "Humanities (3-hours min-6 hours max), 3, ENG 211, B, A, D, 3", "Humanities (3-hours min-6 hours max), 3, ENG 221, F, O, D, 3", "Humanities (3-hours min-6 hours max), 3, ENG 222, S, E, D, 3", "Humanities (3-hours min-6 hours max), 3, ENG 251, F, E, D, 3", "Humanities (3-hours min-6 hours max), 3, ENG 252, S, O, D, 3",
+				"Humanities (3-hours min-6 hours max), 3, FR 101, F, A, D, 3", "Humanities (3-hours min-6 hours max), 3, FR 102, S, A, D, 3, FR 101", "Humanities (3-hours min-6 hours max), 3, FR 201, B, A, D, 3, FR 102", "Humanities (3-hours min-6 hours max), 3, FR 202, B, A, D, 3, FR 201", "Humanities (3-hours min-6 hours max), 3, GR 201, F, A, D, 3", "Humanities (3-hours min-6 hours max), 3, GR 202, S, A, D, 3, GR 201",
+				"Humanities (3-hours min-6 hours max), 3, HEB 201, F, O, D, 3", "Humanities (3-hours min-6 hours max), 3, HEB 202, S, E, D, 3, HEB 201", "Humanities (3-hours min-6 hours max), 3, HIST 101, B, A, D, 3", "Humanities (3-hours min-6 hours max), 3, HIST 102, B, A, D, 3", "Humanities (3-hours min-6 hours max), 3, HIST 121, B, A, D, 3", "Humanities (3-hours min-6 hours max), 3, HIST 122, B, A, D, 3",
+				"Humanities (3-hours min-6 hours max), 3, HIST 203, B, A, D, 3", "Humanities (3-hours min-6 hours max), 3, HUM 221, F, A, D, 3", "Humanities (3-hours min-6 hours max), 3, HUM 222, S, A, D, 3", "Humanities (3-hours min-6 hours max), 3, PHIL 201, F, A, D, 3", "Humanities (3-hours min-6 hours max), 3, PHIL 202, S, A, D, 3", "Humanities (3-hours min-6 hours max), 3, REL 101, B, A, D, 3",
+				"Humanities (3-hours min-6 hours max), 3, REL 103, B, A, D, 3", "Humanities (3-hours min-6 hours max), 3, REL 107, B, A, D, 3", "Humanities (3-hours min-6 hours max), 3, SPAN 101, F, A, D, 3", "Humanities (3-hours min-6 hours max), 3, SPAN 102, S, A, D, 3, SPAN 101", "Humanities (3-hours min-6 hours max), 3, SPAN 106, F, E, D, 3", "Humanities (3-hours min-6 hours max), 3, SPAN 107, S, O, D, 3, SPAN 106",
+				"Humanities (3-hours min-6 hours max), 3, SPAN 201, F, A, D, 3, SPAN 102", "Humanities (3-hours min-6 hours max), 3, SPAN 202, S, A, D, 3, SPAN 201", "Humanities (3-hours min-6 hours max), 3, SPAN 205, F, A, D, 3", "Upper-division Diversity/Global (3-hours), 3, AG 312, B, A, D, 3", "Upper-division Diversity/Global (3-hours), 3, ANTH 303, B, A, D, 3", "Upper-division Diversity/Global (3-hours), 3, ANTH 310, B, A, D, 3",
+				"Upper-division Diversity/Global (3-hours), 3, ANTH 333, S, A, D, 3", "Upper-division Diversity/Global (3-hours), 3, ANTH 340, S, A, D, 3", "Upper-division Diversity/Global (3-hours), 3, ART 305, B, A, D, 3, ENG 104", "Upper-division Diversity/Global (3-hours), 3, BUS 302, F, E, D, 3", "Upper-division Diversity/Global (3-hours), 3, BUS 310, B, A, D, 3", "Upper-division Diversity/Global (3-hours), 3, CDIS 320, B, A, D, 3",
+				"Upper-division Diversity/Global (3-hours), 3, CJ 310, F, A, D, 3", "Upper-division Diversity/Global (3-hours), 3, COMM 310, B, A, D, 3", "Upper-division Diversity/Global (3-hours), 3, COMM 330, B, A, D, 3", "Upper-division Diversity/Global (3-hours), 3, COMM 331, B, A, D, 3", "Upper-division Diversity/Global (3-hours), 3, CTED 304, B, A, D, 3", "Upper-division Diversity/Global (3-hours), 3, DFM 309, B, A, D, 3",
+				"Upper-division Diversity/Global (3-hours), 3, ENG 303, B, A, D, 3", "Upper-division Diversity/Global (3-hours), 3, ENG 378, B, A, D, 3", "Upper-division Diversity/Global (3-hours), 3, ENG 379, B, A, D, 3", "Upper-division Diversity/Global (3-hours), 3, FCS 403, B, A, D, 3", "Upper-division Diversity/Global (3-hours), 3, HIST 305, B, A, D, 3", "Upper-division Diversity/Global (3-hours), 3, HPE 321, B, A, D, 3",
+				"Upper-division Diversity/Global (3-hours), 3, HPE 325, B, A, D, 3", "Upper-division Diversity/Global (3-hours), 3, MUS 375, B, A, D, 3", "Upper-division Diversity/Global (3-hours), 3, NURS 312, B, A, D, 3", "Upper-division Diversity/Global (3-hours), 3, PSCI 312, B, A, D, 3", "Upper-division Diversity/Global (3-hours), 3, PSCI 330, B, A, D, 3", "Upper-division Diversity/Global (3-hours), 3, PSY 312, B, A, D, 3",
+				"Upper-division Diversity/Global (3-hours), 3, REL 402, B, A, D, 3", "Upper-division Diversity/Global (3-hours), 3, SOC 302, B, A, D, 3", "Upper-division Diversity/Global (3-hours), 3, SPAN 303, B, A, D, 3");
 
 		String credit = "";
 		String reqCourse = "";
@@ -276,8 +325,10 @@ public class advisor{
 		List<String> requirmentsList = new ArrayList<String>();
 
 		if(major.equals("CS")){
-			requirmentsList = Arrays.asList("Computer Science Orientation, 1, CS 102, F, A, C, 1", "Discrete Mathematics for Computer Science, 3, CS 220, F, A, C, 3, MATH 119",
-					"Programming Language Concepts, 3, CS 301, F, O, C, 3", "Computer Science II, 4, CS 234, S, A, C, 4", "Database Design and Programming, 3, CS 359, F, E, C, 3, CS 357");
+			requirmentsList = Arrays.asList("Computer Science Orientation, 1, CS 102, F, A, C, 1", "Introduction to Computer Programming, 3, CS 120, F, A, C, 3", "Computer Science I, 4, CS 123, F, A, C, 4", "Discrete Mathematics for Computer Science, 3, CS 220, F, A, C, 3, MATH 119",
+					"Computer Science II, 4, CS 234, S, A, C, 4, CS 123", "Programming Language Concepts, 3, CS 301, F, O, C, 3, CS 123", "Data Structures, 3, CS 357, S, A, C, 3, CS 123", "Database Design and Programming, 3, CS 359, F, E, C, 3, CS 357", "Design and Analysis of Algorithms, 3, CS 451, S, E, C, 3, CS 220",
+					"Operating Systems, 3, CS 461, S, O, C, 3, EET 340", "Software Engineering, 3, CS 472, S, E, C, 3, CS 234", "Logic Circuits, 3, EET 241, F, A, C, 3, MATH 119", "Introduction Computer Organization/Architecture, 3, EET 340, S, A, C, 3, EET 241", "Report Writing, 3, ENG 305, B, A, C, 3, ENG 104", "Calculus I, 3, MATH 124, B, A, C, 3, MATH 120",
+					"Calculus II, 3, Math 132, B, A, C, 3, MATH 132", "Statistical Methods I, 3, STAT 213, B, A, C, 3, MATH 119");
 		}
 
 
@@ -286,7 +337,6 @@ public class advisor{
 			String reqCourse = "";
 			int creditHours = 0;
 			String requirmentOne = requirmentsList.get(0);
-
 			String nameOne = requirmentOne.substring(0, requirmentOne.indexOf(",")); requirmentOne = requirmentOne.substring(requirmentOne.indexOf(",") + 2, requirmentOne.length());
 			String creditNeed = requirmentOne.substring(0, requirmentOne.indexOf(",")); requirmentOne = requirmentOne.substring(requirmentOne.indexOf(",") + 2, requirmentOne.length());
 			String courseName = requirmentOne.substring(0, requirmentOne.indexOf(",")); requirmentOne = requirmentOne.substring(requirmentOne.indexOf(",") + 2, requirmentOne.length());
@@ -307,7 +357,7 @@ public class advisor{
 
 			requirements first = new requirements(nameOne);
 			first.setNeededCredit(creditHoursNeed);
-
+			first.setPriority(2);
 			course temp1 = new course(courseName);
 			temp1.setGrade(grade);
 			temp1.setPreReq(reqCourse);
@@ -355,6 +405,7 @@ public class advisor{
 				} else {
 					requirements next = new requirements(nameOne);
 					next.setNeededCredit(creditHoursNeed);
+					next.setPriority(2);
 
 					course temp2 = new course(courseName);
 					temp2.setGrade(grade);
@@ -394,11 +445,12 @@ public class advisor{
 	}
 
 	/**
-	 * 
-	 * @return
+	 * converts requirements into a string
+	 * @return string of requirements
 	 */
 	public String stringReq() {
 		String s = "";
+
 		for(int i = 0; i < req.size(); i++){
 			s = s + (req.get(i)).getDesc() + "\n";
 			s = s + "\t" + ((req.get(i)).getCourse(0)).getName() + "\n";
@@ -410,8 +462,8 @@ public class advisor{
 	}
 
 	/**
-	 * 
-	 * @return
+	 * converts the array of courses into a string
+	 * @return courses the student has taken
 	 */
 	public String stringCourses() {
 
@@ -461,8 +513,10 @@ public class advisor{
 
 	}
 
-
-
+	/**
+	 * sets the year of the upcoming semester
+	 * @param i
+	 */
 	public void setYear(int i) {
 		year = i;
 		if (i%2 == 0){
@@ -472,6 +526,10 @@ public class advisor{
 		}
 	}
 
+	/**
+	 * sets the upcoming semester fall or spring
+	 * @param s
+	 */
 	public void setSemester(String s) {
 		s = s.toUpperCase();
 		char c = s.charAt(0);
